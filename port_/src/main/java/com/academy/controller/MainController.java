932038fd4477d9d4ef45d441ecb44da0cb725317
송경원 <1612134@hanmail.net>
2023-03-30 21:54:23 +0900

    commit

--- port_/src/main/java/com/academy/controller/MainController.java
+++ port_/src/main/java/com/academy/controller/MainController.java
@@ -4,11 +4,13 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.extern.log4j.Log4j;
 
 @Log4j
 @Controller
+@RequestMapping("/user/*")
 public class MainController {
 	
 	/**
@@ -16,17 +18,26 @@ public class MainController {
 	 * @param request
 	 * @return String
 	 */
-	@GetMapping("/main")
+	@GetMapping("main")
 	public String main(HttpServletRequest request) {
 		return request.getRequestURI();
 	}
+	
+	@GetMapping("login")
+	public String loginPage(HttpServletRequest request) {
+		return request.getRequestURI();
+	}
+	
+	//---------------------------------------------------------
+	// 학원 start
+	//---------------------------------------------------------
 	
 	/**
 	 * 메인 > 학원
 	 * @param request
 	 * @return String
 	 */
-	@GetMapping("/academy")
+	@GetMapping("academy")
 	public String academy(HttpServletRequest request) {
 		return request.getRequestURI();
 	}
@@ -36,18 +47,31 @@ public class MainController {
 	 * @param request
 	 * @return String
 	 */
-	@GetMapping("/academy/academy_detail_pop")
+	@GetMapping("academy/academy_detail_pop")
 	public String academy_detail_pop(HttpServletRequest request) {
 		return request.getRequestURI();
 	}
+	
+	//---------------------------------------------------------
+	// 학원 end
+	//---------------------------------------------------------
+	
+	//---------------------------------------------------------
+	// 마이페이지 start
+	//---------------------------------------------------------
 	
 	/**
 	 * 메인 > 마이페이지 메인
 	 * @param request
 	 * @return String
 	 */
+	@GetMapping("my_page/my_main")
 	public String myMain(HttpServletRequest request) {
 		return request.getRequestURI();
 	}
+	
+	//---------------------------------------------------------
+	// 마이페이지 end
+	//---------------------------------------------------------
 	
 }
