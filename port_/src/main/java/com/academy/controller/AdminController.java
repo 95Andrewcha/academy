6abbad79b6e9d7e38dd807df424018d713a8a889
=junghyun <ae6567@naver.com>
2023-05-15 17:30:37 +0900

    asdassa

--- port_/src/main/java/com/academy/controller/AdminController.java
+++ port_/src/main/java/com/academy/controller/AdminController.java
@@ -99,14 +99,14 @@ public class AdminController {
 	 * 
 	 * @param request
 	 * @return String
-	 */
+	 */ 
 	@RequestMapping("logout")
 	public String logout(HttpServletRequest request, HttpServletResponse response) {
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 
 		if (auth != null) {
 			new SecurityContextLogoutHandler().logout(request, response, auth);
-		}
+		} 
 
 		return "redirect:/admin/login";
 	}
